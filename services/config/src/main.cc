bbf9e2df0b4e7bde10d67603752053beb5ff3521
David Bourgault <dav.bour@gmail.com>
2021-03-19 01:49:30 -0400

    Added MongoDB to third_parties, ConfigService
    + Added MongoDB's C and C++ drivers to `third_party/`.
      As of now, the plan is to only use Mongo for the ConfigService, but
      I've still decided to keep all third parties in a single folder. This
      partially undercuts one of the advantages of a micro-service oriented
      architecture (as far as my current understanding goes) but keeps the
      repo clean(ish)
    - MongoDB is currently included as a shared library, and the Dockerfile
      for the config service has _not_ been updated accordingly: the Docker
      container will fail to run. I need to investigate which of these
      options is preferable : building as static libs or copying (which
      seems complicated? Docker is being annoying about it because of
      symlinks)
    
    + Added arguments to third_party/build.py, to enable selecting which
      third parties to build and adding a clean rebuild switch
    
    + Added configuration protobuf message to configure the config-service.
      This is how I plan to managed configuration for all services (Protobuf
       message in JSON format)

--- services/config/src/main.cc
+++ services/config/src/main.cc
@@ -1,37 +1,112 @@
+#include <fstream>
+#include <sstream>
+#include <string>
 #include <memory>
+#include <filesystem>
 
+#include <google/protobuf/util/json_util.h>
 #include <grpcpp/grpcpp.h>
+
+#include <bsoncxx/builder/stream/array.hpp>
+#include <bsoncxx/builder/stream/document.hpp>
+#include <bsoncxx/builder/stream/helpers.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/database.hpp>
+#include <mongocxx/exception/exception.hpp>
+#include <mongocxx/instance.hpp>
+#include <mongocxx/uri.hpp>
 
 #include "config.pb.h"
 #include "config.grpc.pb.h"
+#include "config.service_config.pb.h"
 
 namespace ganymede::services::config {
 
 class ConfigServiceImpl final : public ConfigService::Service {
+public:
+    ConfigServiceImpl() = delete;
+    ConfigServiceImpl(std::string mongo_uri)
+        : m_mongoclient(mongocxx::uri{mongo_uri})
+        , m_mongodb(m_mongoclient["configurations"])
+    {
+    }
+
     grpc::Status CreateConfig(grpc::ServerContext* context, const CreateConfigRequest* request, CreateConfigResponse* response) {
-        response->set_uid("1");
+        auto collection = m_mongodb["configurations"];
+        auto builder = bsoncxx::builder::stream::document{};
+
+        bsoncxx::document::value doc_value = builder
+        << "uid" << 1
+        << bsoncxx::builder::stream::finalize;
+
+        try {
+            bsoncxx::stdx::optional<mongocxx::result::insert_one> result = collection.insert_one(doc_value.view());
+
+            if (not (result and result.value().result().inserted_count() == 1)) {
+                return grpc::Status(grpc::StatusCode::UNKNOWN, "database error");
+            }
+        } catch (const mongocxx::exception& e) {
+            return grpc::Status(grpc::StatusCode::UNKNOWN, "database error");
+        }
+
         return grpc::Status::OK;
     }
 
     grpc::Status GetConfig(grpc::ServerContext* context, const GetConfigRequest* request, GetConfigResponse* response) {
-        response->set_uid("1");
-        return grpc::Status::OK;
+        return grpc::Status(grpc::StatusCode::UNIMPLEMENTED, "Not yet implemented");
     }
 
     grpc::Status UpdateConfig(grpc::ServerContext* context, const UpdateConfigRequest* request, UpdateConfigResponse* response) {
-        response->set_uid("1");
-        return grpc::Status::OK;
+        return grpc::Status(grpc::StatusCode::UNIMPLEMENTED, "Not yet implemented");
     }
+
+private:
+     mongocxx::client m_mongoclient;
+     mongocxx::database m_mongodb;
 };
 
+
+std::string makeMongoURI(const ConfigServiceConfig::MongoDBConfig& config)
+{
+    std::stringstream ss;
+    ss << "mongodb+srv://" 
+       << config.user() << ":" << config.password() 
+       << "@" << config.host() 
+       << "/" << config.database()
+       << "?retryWrites=true&w=majority";
+
+    return ss.str();
+}
+
+}
+
+std::string readFile(const std::filesystem::path& path)
+{
+    std::ostringstream content("");
+    std::ifstream ifs(path);
+
+    if (ifs) {
+        content << ifs.rdbuf();
+        ifs.close();
+    }
+
+    return content.str();
 }
 
 int main(int argc, const char* argv[])
 {
-    ganymede::services::config::ConfigServiceImpl service;
+    ganymede::services::config::ConfigServiceConfig service_config;
+
+    if (not google::protobuf::util::JsonStringToMessage(readFile("config.service_config.json"), &service_config).ok()) {
+        return -1;
+    }
+
+    std::string mongo_uri = makeMongoURI(service_config.mongodb());
+    ganymede::services::config::ConfigServiceImpl service(mongo_uri);
+    mongocxx::instance instance{};
 
     grpc::ServerBuilder builder;
-    builder.AddListeningPort("0.0.0.0:443", grpc::InsecureServerCredentials());
+    builder.AddListeningPort("0.0.0.0:3000", grpc::InsecureServerCredentials());
     builder.RegisterService(&service);
 
     std::unique_ptr<grpc::Server> server = builder.BuildAndStart();
--- third_party/build.py
+++ third_party/build.py
@@ -1,17 +1,35 @@
 #!/usr/bin/env python3
 '''Script to build third party dependencies'''
 
+import argparse
 import os
+import shutil
 import subprocess
 
 
-def build(submodule, cmake_args=None):
+BUILD_OPTIONS = {
+    'googletest': {},
+    'grpc': {'gRPC_INSTALL': 'ON', 'gRPC_BUILD_TESTS': 'OFF'},
+    'json': {'JSONBuildTests': 'OFF'},
+    'cpp-jwt': {},
+    'mongo-c-driver': {},
+    'mongo-cxx-driver': {}
+}
+
+
+def build(submodule, cmake_args=None, clean=False):
     '''Build submodule'''
     cwd = os.getcwd()
     os.chdir(submodule)
+
+    if clean:
+        shutil.rmtree('cmake/build')
+        shutil.rmtree('cmake/install')
+
     os.makedirs('cmake/build', exist_ok=True)
     os.makedirs('cmake/install', exist_ok=True)
     os.chdir('cmake/build')
+
 
     args = ['cmake', '-GNinja', '-DCMAKE_INSTALL_PREFIX=../install', '../..']
     if cmake_args:
@@ -26,16 +44,26 @@ def build(submodule, cmake_args=None):
     os.chdir(cwd)
 
 
+def comma_seperated_list(values):
+    return values.split(',')
+
+
 def main(args):
     os.environ['CMAKE_PREFIX_PATH'] = ''
-    build('googletest')
-    build('grpc', {'gRPC_INSTALL': 'ON', 'gRPC_BUILD_TESTS': 'OFF'})
-    build('json', {'JSONBuildTests': 'OFF'})
-    build('cpp-jwt')
+
+    if not args.projects:
+        args.projects = BUILD_OPTIONS.keys()
+
+    for project in BUILD_OPTIONS:
+        if project in args.projects:
+            build(project, cmake_args=BUILD_OPTIONS[project], clean=args.clean)
 
     print(f"CMAKE_PREFIX_PATH={os.environ['CMAKE_PREFIX_PATH']}")
     print(f"PATH={os.environ['PATH']}")
 
 
 if __name__ == '__main__':
-    main(None)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--projects', type=comma_seperated_list)
+    parser.add_argument('--clean', action='store_true')
+    main(parser.parse_args())
