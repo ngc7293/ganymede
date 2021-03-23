05f850e60b6d7c0236d147de1897cc9a7f69f4b1
David Bourgault <dav.bour@gmail.com>
2021-03-23 15:58:21 -0400

    Added JWT/domain checking to config service
    common:
    + Added auth functions to common namespace
    
    config:
    + Added JWT checking to all calls
    + Configurations are now filtered by domain. Domain is expected to be
      found as a claim in JWT token (https://davidbourgault.ca/domain)
    
    third parties
    * Changed nlohmann::json checkout to release v3.9.1

--- ganymede/services/config/src/main.cc
+++ ganymede/services/config/src/main.cc
@@ -10,7 +10,7 @@
 #include <mongoc/mongoc.h>
 
 #include <bsoncxx/builder/basic/document.hpp>
-#include <bsoncxx/builder/stream/document.hpp>
+
 #include <mongocxx/client.hpp>
 #include <mongocxx/database.hpp>
 #include <mongocxx/exception/exception.hpp>
@@ -18,12 +18,25 @@
 #include <mongocxx/uri.hpp>
 
 #include "common/status.hh"
+#include "common/auth/jwt.hh"
 
 #include "config.pb.h"
 #include "config.grpc.pb.h"
 #include "config.service_config.pb.h"
 
 #include "bson_serde.hh"
+
+namespace {
+
+bsoncxx::document::value BuildIDFilter(const std::string& id, const std::string& domain)
+{
+    bsoncxx::builder::basic::document builder;
+    builder.append(bsoncxx::builder::basic::kvp("_id", bsoncxx::oid(std::string_view(id))));
+    builder.append(bsoncxx::builder::basic::kvp("domain", domain));
+    return builder.extract();
+}
+
+}
 
 namespace ganymede::services::config {
 
@@ -33,53 +46,71 @@ public:
     ConfigServiceImpl(std::string mongo_uri)
         : m_mongoclient(mongocxx::uri{mongo_uri})
         , m_mongodb(m_mongoclient["configurations"])
+        , m_collection(m_mongodb["configurations"])
     {
     }
 
     grpc::Status CreateConfig(grpc::ServerContext* context, const CreateConfigRequest* request, CreateConfigResponse* response) {
+        std::string domain;
+        if (not common::auth::CheckJWTTokenAndGetDomain(context, domain)) {
+            return common::status::UNAUTHENTICATED;
+        }
+
         if (not request->has_config()){
             return common::status::BAD_PAYLOAD;
         }
 
         auto builder = bsoncxx::builder::basic::document{};
+        builder.append(bsoncxx::builder::basic::kvp("domain", domain));
+
         if (not MessageToBson(request->config(), builder)) {
             return common::status::UNIMPLEMENTED;
         }
 
+        bsoncxx::stdx::optional<mongocxx::result::insert_one> result;
         try {
-            auto collection = m_mongodb["configurations"];
-            bsoncxx::stdx::optional<mongocxx::result::insert_one> result = collection.insert_one(builder.view());
-
-            if (result and result.value().result().inserted_count() == 1) {
-                response->set_uid(result.value().inserted_id().get_oid().value.to_string());
-                return grpc::Status::OK;
-            } else {
-                return common::status::DATABASE_ERROR;
-            }
+             result = m_collection.insert_one(builder.view());
         } catch (const mongocxx::exception& e) {
             std::cerr << "[error]" << e.what() << std::endl;
             return common::status::DATABASE_ERROR;
         }
+
+        if (not result or result.value().result().inserted_count() != 1) {
+            return common::status::DATABASE_ERROR;
+        }
+        response->set_uid(result.value().inserted_id().get_oid().value.to_string());
+        return grpc::Status::OK;
     }
 
     grpc::Status GetConfig(grpc::ServerContext* context, const GetConfigRequest* request, GetConfigResponse* response) {
-        try {
-            auto collection = m_mongodb["configurations"];
-            bsoncxx::stdx::optional<bsoncxx::document::value> result = collection.find_one(bsoncxx::builder::stream::document{} << "_id" << bsoncxx::oid(std::string_view(request->uid())) << bsoncxx::builder::stream::finalize);
+        std::string domain;
+        
+        if (not common::auth::CheckJWTTokenAndGetDomain(context, domain)) {
+            return common::status::UNAUTHENTICATED;
+        }
 
-            if (result) {
-                BsonToMessage(result.value(), *(response->mutable_config()));
-                return grpc::Status::OK;
-            } else {
-                return grpc::Status(grpc::StatusCode::NOT_FOUND, "no such configuration");
-            }
+        bsoncxx::stdx::optional<bsoncxx::document::value> result;
+        try {
+            result = m_collection.find_one(BuildIDFilter(request->uid(), domain));
         } catch (const mongocxx::exception& e) {
-            std::cerr << "[error]" << e.what() << std::endl;
+            std::cerr << "[error] " << e.what() << std::endl;
             return common::status::DATABASE_ERROR;
         }
+
+        if (not result) {
+            return grpc::Status(grpc::StatusCode::NOT_FOUND, "no such configuration");
+        }
+
+        BsonToMessage(result.value(), *(response->mutable_config()));
+        return grpc::Status::OK;
     }
 
     grpc::Status UpdateConfig(grpc::ServerContext* context, const UpdateConfigRequest* request, UpdateConfigResponse* response) {
+        std::string domain;
+        if (not common::auth::CheckJWTTokenAndGetDomain(context, domain)) {
+            return common::status::UNAUTHENTICATED;
+        }
+
         if (not request->has_config()){
             return common::status::BAD_PAYLOAD;
         }
@@ -89,27 +120,25 @@ public:
             return common::status::UNIMPLEMENTED;
         }
 
+        bsoncxx::stdx::optional<mongocxx::result::update> result;
         try {
-            auto collection = m_mongodb["configurations"];
-            bsoncxx::stdx::optional<mongocxx::result::update> result = collection.update_one(
-                bsoncxx::builder::stream::document{} << "_id" << bsoncxx::oid(std::string_view(request->uid())) << bsoncxx::builder::stream::finalize,
-                builder.view()
-            );
-
-            if (result and result.value().modified_count() == 1) {
-                return grpc::Status::OK;
-            } else {
-                return grpc::Status(grpc::StatusCode::NOT_FOUND, "no such configuration");
-            }
+            result = m_collection.update_one(BuildIDFilter(request->uid(), domain), builder.view());
         } catch (const mongocxx::exception& e) {
             std::cerr << "[error]" << e.what() << std::endl;
             return common::status::DATABASE_ERROR;
         }
+
+        if (not result or result.value().modified_count() == 0) {
+            return grpc::Status(grpc::StatusCode::NOT_FOUND, "no such configuration");
+        }
+
+        return grpc::Status::OK;
     }
 
 private:
      mongocxx::client m_mongoclient;
      mongocxx::database m_mongodb;
+     mongocxx::collection m_collection;
 };
 
 
@@ -149,7 +178,6 @@ int main(int argc, const char* argv[])
     }
 
     ganymede::services::config::ConfigServiceConfig service_config;
-
     if (not google::protobuf::util::JsonStringToMessage(readFile(argv[1]), &service_config).ok()) {
         return -1;
     }
